abb3690291491ac0c55755f15354799484879847
joellui <53653635+joellui@users.noreply.github.com>
2021-03-24 23:18:29 +0530

    complete flask
    Complete flask in one Go

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    SECRET_KEY = '784d0fc8ff82cb61beb747712e43fd2e'
+    SQLALCHEMY_DATABASE_URI ='sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True 
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
